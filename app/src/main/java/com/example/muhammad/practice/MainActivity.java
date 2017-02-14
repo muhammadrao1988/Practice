8ea9d8438425c42821aad32b9af8e3668df2fe9b
Muhammad Rao <muhammadrao1988@gmail.com>
2017-02-14 16:25:35 +0500

    Repo created for practice

---
+++ app/src/main/java/com/example/muhammad/practice/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.muhammad.practice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MainActivityBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
